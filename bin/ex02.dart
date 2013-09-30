750c4b6ff3fb442e7f2542b904a52b10ad8e5951
repij <regine-pj@hotmail.com>
2013-09-30 02:56:49 -0400

    add ex02

---
+++ bin/ex02.dart
@@ -0,0 +1,26 @@
+void main() {
+  var x = 5;
+  var y = 7;
+  var z = 8;
+  var reponse1 = x-y/z;
+  print(reponse1);
+  
+  
+  var rcarre = 10e2;
+  var pi = 3.14;
+  var radius = pi*rcarre;
+  print(radius);
+
+  
+  var squarefeet = 220*260;
+  var acres = 43.560;
+  var size = squarefeet/acres;
+  print (size);
+  
+  
+   var meter = 100;
+   var seconds = 9.58;
+   var speed = meter/seconds;
+   print (speed);
+      
+}
